f5d45939cc8eccce3107d4e17ddb8d46387cc796
BuildTools <unconfigured@null.spigotmc.org>
2022-01-14 11:45:52 +0100

    IntybusSprite.cs

---
+++ Intybus/Graphics/IntybusSprite.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Intybus.Graphics
+{
+    public class IntybusSprite
+    {
+        public Color Tint;
+        
+        public bool FlipX;
+        public bool FlipY;
+    }
+}
